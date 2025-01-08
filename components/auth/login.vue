0944d02338d12532d53a7691a1a1d86ba4b4ed16
Navod kalhara <87455453+Navod1997@users.noreply.github.com>
2025-01-08 15:41:22 +0530

    register component create

--- components/auth/login.vue
+++ components/auth/login.vue
@@ -1,46 +1,5 @@
 <template>
-  <section class="flex items-center justify-center min-h-screen bg-gray-100">
-    <!-- Login Card -->
-    <div class="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-      <!-- Title -->
-      <h2 class="text-2xl font-bold text-center text-gray-800">Login</h2>
-
-      <!-- Form -->
-      <form @submit.prevent="handleLogin">
-        <!-- Username -->
-        <div>
-          <label class="block text-sm font-medium text-gray-700">Username</label>
-          <input
-            type="text"
-            v-model="username"
-            placeholder="Enter your username"
-            required
-            class="w-full p-3 mt-1 border rounded-lg focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-          <label class="block text-sm font-medium text-gray-700">Password</label>
-          <input
-            type="password"
-            v-model="password"
-            placeholder="Enter your password"
-            required
-            class="w-full p-3 mt-1 border rounded-lg focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </div>
-
-        <!-- Login Button -->
-        <button
-          type="submit"
-          class="w-full px-4 py-2 mt-6 font-semibold text-white bg-indigo-600 rounded-lg hover:bg-indigo-500"
-        >
-          Login
-        </button>
-      </form>
-    </div>
-  </section>
+ 
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,124 @@
 <template>
-  <section>
-    <!--   <div @click="showComp='login'" class="cursor-pointer">Login</div>
-  <div @click="showComp='changelogin'" class="cursor-pointer">changelogin</div>
-    <hr class="mt-12">
-   <login v-if="showComp=='login'"/>
-   <changelogin v-if="showComp=='changelogin'"/> -->
-    
-  </section>
+  <div class="min-h-screen p-6 ">
+    <!-- Header -->
+    <div class="flex flex-col items-start justify-between mb-6 sm:flex-row sm:items-center">
+      <h2 class="mb-4 text-2xl font-bold text-gray-800 sm:mb-0">Cashflow</h2>
+      <NuxtLink to="/user/login">
+        <button class="px-4 py-2 text-white rounded-lg hover:bg-indigo-500 btn">
+          Login
+        </button>
+      </NuxtLink>
+
+    </div>
+
+    <!-- Stats Cards -->
+    <div class="grid grid-cols-2 gap-6 mb-6 sm:grid-cols-2 lg:grid-cols-4">
+      <div class="p-4 bg-white rounded-lg shadow">
+        <p class="text-sm text-gray-500">Revenue</p>
+        <p class="text-2xl font-bold">LKR:405,091.00</p>
+        <p class="text-sm text-green-500">+4.75%</p>
+      </div>
+      <div class="p-4 bg-white rounded-lg shadow">
+        <p class="text-sm text-gray-500">Overdue Invoices</p>
+        <p class="text-2xl font-bold">LKR:12,787.00</p>
+        <p class="text-sm text-red-500">-5.02%</p>
+      </div>
+      <div class="p-4 bg-white rounded-lg shadow">
+        <p class="text-sm text-gray-500">Outstanding Invoices</p>
+        <p class="text-2xl font-bold">LKR:245,988.00</p>
+        <p class="text-sm text-green-500">+1.39%</p>
+      </div>
+      <div class="p-4 bg-white rounded-lg shadow">
+        <p class="text-sm text-gray-500">Expenses</p>
+        <p class="text-2xl font-bold">LKR:30,156.00</p>
+        <p class="text-sm text-red-500">-10.81%</p>
+      </div>
+    </div>
+
+    <!-- Recent Activity -->
+    <div class="mb-6">
+      
+      <h3 class="mb-4 text-lg font-semibold text-gray-800">Recent Activity</h3>
+      <div class="grid grid-cols-3 gap-3">
+        <div class="p-4 bg-white rounded-lg shadow">
+          <div class="divide-y divide-gray-200">
+            <div class="flex flex-col items-start justify-between py-3 sm:flex-row sm:items-center">
+              <div>
+                <p class="text-gray-700">LKR:7,600.00 USD</p>
+                <p class="text-sm text-gray-500">Paid - Reform</p>
+              </div>
+              <p class="mt-2 text-sm text-green-500 sm:mt-0">Paid</p>
+            </div>
+            <div class="flex flex-col items-start justify-between py-3 sm:flex-row sm:items-center">
+              <div>
+                <p class="text-gray-700">LKR:10,000.00 USD</p>
+                <p class="text-sm text-gray-500">Withdraw - Tom Cook</p>
+              </div>
+              <p class="mt-2 text-sm text-gray-500 sm:mt-0">Withdraw</p>
+            </div>
+            <div class="flex flex-col items-start justify-between py-3 sm:flex-row sm:items-center">
+              <div>
+                <p class="text-gray-700">LKR:2,000.00 USD</p>
+                <p class="text-sm text-gray-500">Overdue - Tuple</p>
+              </div>
+              <p class="mt-2 text-sm text-red-500 sm:mt-0">Overdue</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 bg-white rounded-lg shadow">
+          <div class="divide-y divide-gray-200">
+            <div class="flex flex-col items-start justify-between py-3 sm:flex-row sm:items-center">
+              <div>
+                <p class="text-gray-700">LKR:7,600.00 USD</p>
+                <p class="text-sm text-gray-500">Paid - Reform</p>
+              </div>
+              <p class="mt-2 text-sm text-green-500 sm:mt-0">Paid</p>
+            </div>
+            <div class="flex flex-col items-start justify-between py-3 sm:flex-row sm:items-center">
+              <div>
+                <p class="text-gray-700">LKR:10,000.00 USD</p>
+                <p class="text-sm text-gray-500">Withdraw - Tom Cook</p>
+              </div>
+              <p class="mt-2 text-sm text-gray-500 sm:mt-0">Withdraw</p>
+            </div>
+            <div class="flex flex-col items-start justify-between py-3 sm:flex-row sm:items-center">
+              <div>
+                <p class="text-gray-700">LKR:2,000.00 USD</p>
+                <p class="text-sm text-gray-500">Overdue - Tuple</p>
+              </div>
+              <p class="mt-2 text-sm text-red-500 sm:mt-0">Overdue</p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+    <!-- Recent Clients -->
+    <div>
+      <h3 class="mb-4 text-lg font-semibold text-gray-800">Recent Clients</h3>
+      <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div class="p-4 bg-white rounded-lg shadow">
+          <p class="text-sm text-gray-500">Tuple</p>
+          <p class="text-lg font-bold">LKR:2,000.00</p>
+          <p class="text-sm text-red-500">Overdue</p>
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow">
+          <p class="text-sm text-gray-500">SavvyCal</p>
+          <p class="text-lg font-bold">LKR:14,000.00</p>
+          <p class="text-sm text-green-500">Paid</p>
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow">
+          <p class="text-sm text-gray-500">Reform</p>
+          <p class="text-lg font-bold">LKR:7,600.00</p>
+          <p class="text-sm text-green-500">Paid</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
 
 <script>
 //import textInput from '~/components/customcontrol/textinput'
@@ -31,6 +142,12 @@ export default {
     };
   },
   methods: {
+    goToLogin() {
+      this.$router.push('/user/login'); // Navigate to /user/login
+    },
+    goToChangeLogin() {
+      this.$router.push('/user/changelogin'); // Navigate to /user/changelogin
+    },
     handleSignup() {
       // Simulate signup logic
       if (this.email && this.accountName) {
@@ -52,5 +169,7 @@ export default {
 </script>
 
 <style scoped>
-/* Custom Styles */
+  .btn{
+    background-color: #ed9135; 
+  }
 </style>
