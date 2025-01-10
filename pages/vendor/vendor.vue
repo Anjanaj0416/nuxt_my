6f74fd06aa9368a60f71079a8bdbb2785917c403
Navod kalhara <87455453+Navod1997@users.noreply.github.com>
2025-01-10 14:33:51 +0530

    update

--- pages/vendor/vendor.vue
+++ pages/vendor/vendor.vue
@@ -4,7 +4,7 @@
           <h1 class="mt-8 text-4xl font-semibold tracking-tight text-gray-700 text-balance sm:text-5xl">Vender</h1>
           <div class='mt-10'>
             <button
-            @click="openModal('add')"
+            @click="openModal('view')"
               class="px-4 py-2 font-semibold text-blue-900 transition duration-300 ease-in-out border-2 border-blue-800 rounded-lg hover:bg-blue-900 hover:text-white focus:ring-2 focus:ring-blue-600 focus:outline-none"
               aria-label="Add a new vendor"
             >
@@ -75,27 +75,6 @@
                                     Restore Vendor
                                 </button>
                             </td>
-
-                            <!-- Add Vendor Modal -->
-                            <div v-if="isModalOpen.view" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 items-center justify-center w-full h-full overflow-y-auto bg-opacity-50 backdrop-blur-sm">
-                                <div class="absolute relative w-full max-w-4xl p-4 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        <!-- Close button -->
-                                        <button type="button" @click="closeModal('add')" class="absolute inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg top-3 right-3 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
-                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                            </svg>
-                                        </button>
-
-                                        <!-- Modal content -->
-                                        <div class="p-6 md:p-8">
-                                            <addvender />
-
-                                            <p>ff</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
             
                             <!-- View Modal -->
                             <div v-if="isModalOpen.view" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 items-center justify-center w-full h-full overflow-y-auto bg-opacity-50 backdrop-blur-sm">
@@ -168,11 +147,9 @@
   import headercomp from '~/components/header';
   import footercomp from '~/components/footer';
   import viewvender from '~/pages/vendor/viewvendor.vue'
-  import addvender from '~/pages/vendor/addvendor.vue'
-
   
   export default {
-    components: { headercomp, footercomp, viewvender,addvender },
+    components: { headercomp, footercomp, viewvender },
     data() {
       return {
         isModalOpen: {
