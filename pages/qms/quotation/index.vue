6387caeacce8982032834ab114db546d1a4db5ad
thusharasandakalum <thusharasandakalum@gmail.com>
2025-05-11 15:05:33 +0530

    Change s up to May11

--- pages/qms/quotation/index.vue
+++ pages/qms/quotation/index.vue
@@ -1,22 +1,24 @@
 <template>
-  <div>
-    <section class="justify-center min-h-screen px-4 mt-24 lg:px-80">
-      <div class="text-2xl uppercase">Quotations</div>
-      <div class="flex flex-col items-center justify-between mt-2 md:flex-row">
-        <div class="w-full mb-4 md:mb-0">
-          <div class="mr-2">
-            <Button 
-              class="w-24" 
-              label="New" 
-              variant="primary" 
-              @click="GoToAddNew" 
-            />
-          </div>
-        </div>
-        <div class="w-full md:w-auto">
-          <SearchComp @DoSearch="GetSearch" />
+
+<section class="justify-center min-h-screen px-4 mt-24 mb-20 lg:px-80">
+    <div class="text-2xl uppercase">Vendors</div>
+    <div
+      class="flex flex-col items-center justify-between mt-2 mb-8 md:flex-row"
+    >
+      <div class="w-full mb-4 md:mb-0">
+        <div class="mr-2">
+          <Button
+            class="w-24"
+            label="New"
+            variant="primary"
+            @click="GoToAddNew"
+          />
         </div>
       </div>
+      <div class="w-full md:w-auto">
+        <SearchComp @DoSearch="GetSearch" />
+      </div>
+    </div>
 
       <FilterTab @selected="SetSelectedFilter" :arrFilter="arrFilter" />
 
@@ -129,7 +131,7 @@
       <ApproveView v-if="isApproving" @close="CloseApprovingView()"  />
       <AddEdit v-if="isAddEdit" @close="isAddEdit = false" />
     </section>
-  </div>
+ 
 </template>
 
 <script>
@@ -178,13 +180,13 @@ export default {
     this.quotationStore = useQuotationStore();
     this.showLoading = this.$showLoading;
 
-    await this.quotationStore.loadListQuotations({
-      keyword: "",
-      searchBy: this.searchBy,
-    },this.showLoading);
+    //await this.quotationStore.loadListQuotations({
+    //   keyword: "",
+    //   searchBy: this.searchBy,
+    // },this.showLoading);
    
-    await this.quotationStore.loadInitQuotation(this.showLoading);
-    this.imageroot = this.quotationStore.initQuotation.baseUrl;
+    //await this.quotationStore.loadInitQuotation(this.showLoading);
+   // this.imageroot = this.quotationStore.initQuotation.baseUrl;
    
 
  
@@ -193,7 +195,7 @@ export default {
 
     GoToAddNew() {
       this.quotationStore.ResetQuotation();
-      this.isAddEdit = true; // Ensure modal state updates
+      this.isAddEdit = true;
     },
 
     async GoToViewAllQuoVer() {
