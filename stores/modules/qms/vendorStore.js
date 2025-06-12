b885b8f86079cec71290c695c77894a1ccc661c7
Navod Kalhara <navodkalhara0@gmail.com>
2025-06-12 17:29:19 +0530

    regvendoradmin update

--- stores/modules/qms/vendorStore.js
+++ stores/modules/qms/vendorStore.js
@@ -11,6 +11,14 @@ export const useVendorStore = defineStore("vendorStore", {
     listLeads:[],
     InitLeads:{},
     curLead:{},
+    obgRegVendorAdmin:{
+      suAdmin:{
+
+      },
+      otherUswe:{
+
+      }
+    },
   }),
   persist: true,
 
