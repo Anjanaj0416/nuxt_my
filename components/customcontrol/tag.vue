a5145c7ef3b7ba61c76913e672cbe6c67697ace8
Thilini Perera <84263208+thili-perera@users.noreply.github.com>
2025-11-25 09:08:31 +0530

    granted issue

--- components/customcontrol/tag.vue
+++ components/customcontrol/tag.vue
@@ -15,29 +15,21 @@ tag
 
 
 <template>
-  <section >
+  <section>
     <div class="csstag relative">
- <div class="cssbtn absolute top-0 right-0 border-gray-500 rounded p-2 text-blue-700 hover:bg-red-600 hover:text-white rounded-full -mr-2  -mt-2 cursor-pointer" @click="del_tag">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-3 w-3"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
+      <div
+        class="cssbtn absolute top-0 right-0 border-gray-500 rounded p-2 text-blue-700 hover:bg-red-600 hover:text-white rounded-full -mr-2  -mt-2 cursor-pointer"
+        @click="del_tag">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
       </div>
 
-    <div class="truncate text-gray-600 w-32 " title="">
-        <div v-show="!istageditable || !isedit" @click="isedit='true'" class="truncate" :title="value"> {{value}}     </div>
-        <div v-show="isedit && istageditable"> <input  class=" bg-transparent  px-1"  :value="value"  @input="updateValue($event.target.value)" type="text" @blur="isedit=false"></div>
-    </div>
+      <div class="truncate text-gray-600 w-32 " title="">
+        <div v-show="!istageditable || !isedit" @click="isedit = 'true'" class="truncate" :title="value"> {{ value }} </div>
+        <div v-show="isedit && istageditable"> <input class=" bg-transparent  px-1" :value="value"
+            @input="updateValue($event.target.value)" type="text" @blur="isedit = false"></div>
+      </div>
 
     </div>
   </section>
@@ -45,20 +37,20 @@ tag
 
 <script>
 export default {
-  props: ['value','istageditable'],
+  props: ['value', 'istageditable'],
   components: {},
-   data() {
+  data() {
     return {
-    isedit:false,
+      isedit: false,
     }
   },
   methods: {
-     updateValue: function (value) {
+    updateValue: function (value) {
       this.$emit('input', value.trim())
     },
-    del_tag(){  
+    del_tag() {
 
-       this.$emit('deletetag', this.value)
+      this.$emit('deletetag', this.value)
     }
 
   },
@@ -67,10 +59,11 @@ export default {
 
 <style>
 .cssbtn:hover {
-  @apply transform  scale-125 duration-500;
+  @apply transform scale-125 duration-500;
 }
+
 .csstag {
-  @apply  rounded px-2 h-6 bg-white border-2 border-gray-300 ;
+  @apply rounded px-2 h-6 bg-white border-2 border-gray-300;
   min-width: 100px;
 }
 </style>
