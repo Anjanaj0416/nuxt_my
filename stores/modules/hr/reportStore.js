d4f8a35cfcdc34c493d452b0e5578c9f5f7ab674
Thilini Perera <84263208+thili-perera@users.noreply.github.com>
2026-01-13 07:53:47 +0530

    changes

--- stores/modules/hr/reportStore.js
+++ stores/modules/hr/reportStore.js
@@ -430,6 +430,97 @@ export const useReportStore = defineStore("reportStore", {
         loadingAlert.close();
     },
 
+    async getMonthEndAttendanceSheetType1(req, showLoading) {
+        console.log('API-getMonthEndAttendanceSheetType1:',req);
+
+        const loadingAlert = showLoading("");
+        try {
+          const response = await axios.get(
+            `${import.meta.env.VITE_API_URL}/hr/Report/GetMonthEndAttendanceSheetType1`,
+            {params: {
+              year:req.Year,
+              monthNo:req.Month
+            },
+            responseType: 'blob' 
+          }); 
+
+            const blob = new Blob([response.data], {
+              type: 'application/x-rar-compressed',
+            });
+
+            const url = URL.createObjectURL(blob);
+
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `DailyAttendance_${req.EmpNo}_${req.Year}_${req.Month}.rar`;
+            document.body.appendChild(link);
+            link.click();
+
+            // Clean-up
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+
+          console.log("response:", response);
+          if (response.data.isSuccess) {
+          } else {
+            console.error("Loading error:", response.data.message);
+            // this.showToast(response.data.message, 'error');
+          }
+        } catch (error) {
+          console.error("Loading error:", error);
+          if (error.response && error.response.status == 400) {
+          }
+          // this.showToast(error.response.data.Message, 'error');
+        }
+        loadingAlert.close();
+    },
+
+    async getMonthEndAttendanceSheetType2(req, showLoading) {
+        console.log('API-getMonthEndAttendanceSheetType2:',req);
+
+        const loadingAlert = showLoading("");
+        try {
+          const response = await axios.get(
+            `${import.meta.env.VITE_API_URL}/hr/Report/GetMonthEndAttendanceSheetType2`,
+            {params: {
+              EmpNo: req.EmpNo,
+              year:req.Year,
+              monthNo:req.Month
+            },
+            responseType: 'blob' 
+          }); 
+
+            const blob = new Blob([response.data], {
+              type: 'application/x-rar-compressed',
+            });
+
+            const url = URL.createObjectURL(blob);
+
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `DailyAttendance_${req.EmpNo}_${req.Year}_${req.Month}.rar`;
+            document.body.appendChild(link);
+            link.click();
+
+            // Clean-up
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+
+          console.log("response:", response);
+          if (response.data.isSuccess) {
+          } else {
+            console.error("Loading error:", response.data.message);
+            // this.showToast(response.data.message, 'error');
+          }
+        } catch (error) {
+          console.error("Loading error:", error);
+          if (error.response && error.response.status == 400) {
+          }
+          // this.showToast(error.response.data.Message, 'error');
+        }
+        loadingAlert.close();
+    },
+
 
     async showToast(message, type) {
       const Swal = (await import("sweetalert2")).default;
